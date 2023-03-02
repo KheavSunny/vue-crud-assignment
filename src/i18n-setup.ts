e2d94e1e8cc4177659a8edae54e68b6200fedcaf
Sunnyy13 <kheavsunny0521@gmail.com>
2023-03-02 14:29:04 +0700

    refactor code

--- src/i18n-setup.ts
+++ src/i18n-setup.ts
@@ -1,31 +1,6 @@
 import { nextTick } from 'vue'
 import { createI18n } from "vue-i18n"
 
-// const messages = {
-//   en: {
-//       hello: 'Hello'
-//   },
-//   km: {
-//       hello: 'ជំរាបសួរ'
-//   }
-// }
-
-export const SUPPORT_LOCALES = ['en', 'km']
-
-// const lang = localStorage.getItem('lang') || 'en';
-// const messages = await import(`./locales/${lang}.json`);
-// console.log(messages);
-
-// const i18n = createI18n({
-//   locale: lang, //Get Data From seleted
-//   fallbackLocale: 'en',
-//   messages
-// })
-
-// export {
-//   i18n,
-// }
-
 const locale = sessionStorage.getItem('lang') || 'en'
 
 export function setupI18n(options = { locale: locale }) {
